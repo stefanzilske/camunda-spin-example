da3ce2a78ab309e52cf8adb2941e3cbb1c896fd0
Stefan Zilske <stefan.zilske@holisticon.de>
2017-04-28 08:30:12 +0200

    Initial version

---
+++ src/main/java/de/holisticon/spin/example/SpinExample.java
@@ -0,0 +1,14 @@
+package de.holisticon.spin.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by stefanzilske on 28.04.17.
+ */
+@SpringBootApplication
+public class SpinExample {
+    public static void main(String[] args) {
+        SpringApplication.run(SpinExample.class, args);
+    }
+}
